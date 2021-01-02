f2890dc74b4fe9b17897f177142a7c8aa9476fbe
Adheena Chandran <76645871+adneena@users.noreply.github.com>
2021-01-02 13:53:30 +0530

    Update git_users.py

--- git_users.py
+++ git_users.py
@@ -1,20 +1,29 @@
 import requests
 import json
 import csv
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-def list_of_complete_users():
+def list_of_complete_users(): #returns complete git users details
     user_data=requests.get(f'https://api.github.com/users').json()
     return user_data
 
-def particular_users(login):
+def particular_users(login): #returns particular user details
     particular_user_data = requests.get(f'https://api.github.com/users/{login}').json()
     return particular_user_data
 
-def followers_list(login):
+def followers_list(login): #returns particular users followers
     followers_data = requests.get(f'https://api.github.com/users/{login}/followers').json()
     return followers_data
 
-
+def upload_file_to_drive(): #function to upload csv to drive
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() #client_secrets.json need to be in the same directory as the script
+    drive = GoogleDrive(gauth)
+    file = drive.CreateFile({"mimeType": "text/csv"})
+    file.SetContentFile(r"C:\Users\Aradh\Desktop\users_report_task\users_list.csv")
+    file.Upload()
+    
 all_user_data = list_of_complete_users()
 rows = []
 for user in all_user_data:
@@ -26,7 +35,9 @@ for user in all_user_data:
             rows.append([user["id"],user["login"],single_user_data["name"],followers["id"],followers['login']])
 
 
-with open("users_list.csv", 'w') as csvfile:
+with open("users_list.csv", 'w') as csvfile: #conversion to csv file
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['UserId', 'UserLogin', 'UserName', 'FollowerId', 'FollowerLogin'])
     csvwriter.writerows(rows)
+    
+upload_file_to_drive()
